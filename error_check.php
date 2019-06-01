0ccbb3e001787629020f2dd16e7b27f38b55ce9c
Go-Gunji <go.eliteforce@gmail.com>
2019-06-02 00:47:07 +0900

    画像アップデート追加

--- error_check.php
+++ error_check.php
@@ -1,14 +1,42 @@
 <?php
     header('Content-type: text/plain; charset= UTF-8');
-    if(isset($_POST['store_name'])){
-        $store_name = $_POST['store_name'];
-        $curry_name = $_POST['curry_name'];
-        $hot_level=$_POST['hot_level'];
+    if(!empty($_POST['store_name'])
+    && !empty($_POST['curry_name'])
+    && !empty($_POST['hot_level'])
+    && !empty($_POST['impression'])
+    && !empty($_POST['address'])
+    ){
+        $imginfo = getimagesize($_FILES['file']['tmp_name']);
+        if($imginfo['mime'] == 'image/jpeg'){ $extension = ".jpg"; }
+        if($imginfo['mime'] == 'image/png'){ $extension = ".png"; }
+        if($imginfo['mime'] == 'image/gif'){ $extension = ".gif"; }
 
-        $str = '店名:'.$store_name.'カレーの名前:'.$curry_name.'辛さ:'.$hot_level;
-        $result = nl2br($str);
-        echo $result;
-    }else{
-        echo 'FAIL TO AJAX REQUEST';
+        if(!empty($extension)){
+			
+            /* 画像登録処理 */
+            $file_tmp = $_FILES['file']['tmp_name'];
+            $file_name = $_POST['curry_name']. $extension; // アップロード時のファイル名を設定
+            $file_save = "./img/" . $file_name; // アップロード対象のディレクトリを指定
+            move_uploaded_file($file_tmp, $file_save); // アップロード処理
+        
+            echo "success"; // jquery側にレスポンス
+            
+          } else {
+            
+            echo "fail"; // jquery側にレスポンス
+            
+          }
     }
+    // $store_name = $_POST['store_name'];
+        // $curry_name = $_POST['curry_name'];
+        // $hot_level=$_POST['hot_level'];
+        // $impression = $_POST['impression'];
+        // $address = $_POST['address'];
+
+
+        // $str = '店名:'.$store_name.'カレーの名前:'.$curry_name.'辛さ:'.$hot_level.'感想:'.$impression.'住所:'.$address;
+        // $result = nl2br($str);
+        // echo $result;
+    // }else{
+    //     echo 'FAIL TO AJAX REQUEST';
 ?>
--- js/post.js
+++ js/post.js
@@ -1,63 +1,89 @@
-//マップAPI
-$('.custom-file-input').on('change',function(){
-    $(this).next('.custom-file-label').html($(this)[0].files[0].name);
-  })
+var $ = Y.useJQuery();
+var map = new Y.Map("map");
+map.drawMap(new Y.LatLng(33.590184, 130.401689), 15);
+map.addControl(new Y.LayerSetControl());
+map.addControl(new Y.SliderZoomControlVertical());
+map.addControl(new Y.SearchControl());
+map.SearchControl
 
-
-  var $ = Y.useJQuery();
-  var map = new Y.Map("map");
-  map.drawMap(new Y.LatLng(33.590184,130.401689), 15);
-  map.addControl(new Y.LayerSetControl());
-  map.addControl(new Y.SliderZoomControlVertical());
-  map.addControl(new Y.SearchControl());
-  map.SearchControl
-
-  //クリックイベントを設定
-map.bind("click", function(latlng){onClicked(latlng);});
+//クリックイベントを設定
+map.bind("click", function (latlng) { onClicked(latlng); });
 
 //クリックイベントを定義
-function onClicked(latlng){
+function onClicked(latlng) {
   //クリック位置の緯度経度を指定して、リバースジオコーディングを実行
   var geocoder = new Y.GeoCoder();
-  
-  geocoder.execute( { latlng :latlng} , function( result ) {
+
+  geocoder.execute({ latlng: latlng }, function (result) {
     map.clearFeatures();
-      if ( result.features.length > 0 ) {
-          //リバースジオコーディング結果を表示
-          document.getElementById('address').innerHTML = result.features[0].property.Address;
-          var label = new Y.Label(latlng, "ここ！");
-          map.addFeature(label);
-      }
+    console.log(result.features[0].property.Address);
+    if (result.features.length > 0) {
+      //リバースジオコーディング結果を表示
+      document.getElementById("address").value = result.features[0].property.Address;
+      var label = new Y.Label(latlng, "ここ！");
+      map.addFeature(label);
+    }
   });
 }
 
 $(function () {
-  //投稿ボタン押下
-  $('#postdata').on('click',function(){
-    console.log($('input[name="hot_level"]:checked').val());
-    $.ajax({
-      url:'../error_check.php',
-      type:'POST',
-      data:{
-          'store_name':$('#store_name').val(),
-          'curry_name':$('#curry_name').val(),
-          'hot_level':$('input[name="hot_level"]:checked').val()
-      }
-  })
-  // Ajaxリクエストが成功した時発動
-  .done( (data) => {
-      $('.result').html(data);
-      console.log(data);
-  })
-  // Ajaxリクエストが失敗した時発動
-  .fail( (data) => {
-      $('.result').html(data);
-      console.log(data);
-  })
-  // Ajaxリクエストが成功・失敗どちらでも発動
-  .always( (data) => {
 
-  });
+  $('.custom-file-input').on('change',function(){
+    $(this).next('.custom-file-label').html($(this)[0].files[0].name);
+  })
+  //投稿ボタン押下
+  $('#postdata').on('click', function () {
+
+    // 入力値の取得
+    var store_name = $('#store_name').val(); //店名
+    var curry_name = $('#curry_name').val(); //カレーの名前
+    var hot_level = $('input[name="hot_level"]:checked').val(); //辛さ
+    var impression = $('#impression').val(); //感想
+    var address = $('#address').val(); //住所
+
+    // 必須チェック
+    if (store_name != "" && curry_name != "" && hot_level != "" && impression != "" && address != "") {
+      // イメージファイルセット
+      var fd = new FormData();
+      // var fd = new FormData($('.form-group').get(0));
+      fd.append("file",$("#customFile").prop("files")[0]);
+      for (value of fd.entries()) { 
+        console.log(value); 
+    }
+
+      // その他入力データのセット
+      fd.append('store_name', store_name);
+      fd.append('curry_name', curry_name);
+      fd.append('hot_level', hot_level);
+      fd.append('impression', impression);
+      fd.append('address', address);
+
+      
+    }
+
+    $.ajax({
+      url: '../error_check.php',
+      type: 'POST',
+      data: fd,
+      cache: false,
+      contentType: false,
+      processData: false,
+      dataType: 'html'
+    })
+      // Ajaxリクエストが成功した時発動
+      .done((data) => {
+        $('.result').html(data);
+        console.log(data);
+      })
+      // Ajaxリクエストが失敗した時発動
+      .fail((data) => {
+        $('.result').html(data);
+        console.log(data);
+      })
+      // Ajaxリクエストが成功・失敗どちらでも発動
+      .always((data) => {
+
+      });
 
   });
 });
